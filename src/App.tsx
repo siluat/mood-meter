718bec79cf1c5ee3087f17a9505cecc2307333dc
siluat <siluat@gmail.com>
2023-09-05 00:19:37 +0900

    🌱 Panda CSS 🐼

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
+import { FormEvent } from 'react'
+import { hstack } from '../styled-system/patterns'
+import { css } from '../styled-system/css'
 
 function App() {
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
     <>
-      <h1>Mood Meter</h1>
+      <h1 className={css({ margin: '1rem' })}>Mood Meter</h1>
+      <form onSubmit={onSubmit} className={hstack()}>
+        <input placeholder="이름을 입력하고 입장하세요" />
+        <button type="submit">ENTER</button>
+      </form>
     </>
   )
 }
