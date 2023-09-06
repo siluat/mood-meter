2b76a227823dbf146eb65c6f6cbdf5f9ba7d644d
siluat <siluat@gmail.com>
2023-09-06 23:58:57 +0900

    💄 다크모드에서도 사용자별 무드 상태가 잘 보이도록 수정

--- src/components/UserStates.tsx
+++ src/components/UserStates.tsx
@@ -1,3 +1,4 @@
+import { css } from '../../styled-system/css'
 import { UserState } from '../App'
 
 interface Props {
@@ -14,7 +15,17 @@ export default function UserState({ userStates }: Props) {
           <p key={state.userId}>
             <span>{state.username}: </span>
             {state.moodList.map((mood) => (
-              <span key={mood.ko} style={{ color: mood.color }}>
+              <span
+                key={mood.ko}
+                style={{ color: mood.color }}
+                className={css({
+                  p: '0.1rem',
+                  '@media (prefers-color-scheme: dark)': {
+                    bg: 'white',
+                    borderRadius: '0.1rem',
+                  },
+                })}
+              >
                 {`${mood.ko} (${mood.en})`}
               </span>
             ))}
