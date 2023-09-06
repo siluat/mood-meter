38c5ca9cd81bcd7e2186e2fb816c324c6388567d
siluat <siluat@gmail.com>
2023-09-06 23:05:29 +0900

    🥅 사용자 상태 목록 렌더링시 더 적절한 구조로 HTML 사용

--- src/components/UserStates.tsx
+++ src/components/UserStates.tsx
@@ -6,21 +6,19 @@ interface Props {
 
 export default function UserState({ userStates }: Props) {
   return (
-    <div>
+    <>
       {userStates.map((state) => {
+        if (!state.mood) return null
+
         return (
-          <>
-            {state.mood ? (
-              <p key={state.userId}>
-                <span>{state.username}: </span>
-                <span style={{ color: state.mood.color }}>
-                  {`${state.mood.ko} (${state.mood.en})`}
-                </span>
-              </p>
-            ) : null}
-          </>
+          <p key={state.userId}>
+            <span>{state.username}: </span>
+            <span style={{ color: state.mood.color }}>
+              {`${state.mood.ko} (${state.mood.en})`}
+            </span>
+          </p>
         )
       })}
-    </div>
+    </>
   )
 }
